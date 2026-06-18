5a42eb18d83f081f108bc2b9d923da77e9f0e930
devops4sure <bharathcm204@gmail.com>
2026-06-18 16:24:59 +0530

    added menu-service entity and pom file

---
+++ services/menu-service/src/main/java/com/hostelfoodreview/menuservice/entity/MealType.java
@@ -0,0 +1,7 @@
+package com.hostelfoodreview.menuservice.entity;
+
+public enum MealType {
+    BREAKFAST,
+    LUNCH,
+    DINNER
+}
---
+++ services/menu-service/src/main/java/com/hostelfoodreview/menuservice/entity/Menu.java
@@ -0,0 +1,39 @@
+package com.hostelfoodreview.menuservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "menus")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MealType mealType;
+
+    @Column(nullable = false)
+    private String items;
+
+    @Column(nullable = false)
+    private String createdBy;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
